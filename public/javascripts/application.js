e334e8e4076147eeb51b93d7ac30e88e55b218f0
Morgan Roderick <morgan@MorgBook.local>
2008-10-24 18:50:29 +0200

    Removing unnessary comments from javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,3 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
 if ( /AppleWebKit|MSIE/.test( navigator.userAgent ) ) {
 	Event.addBehavior({
 		'form:submit' : function( e ){
