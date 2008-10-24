ba971ea2e8b0605cd3a83f07d9f1f9aad087195c
Morgan Roderick <morgan@MorgBook.local>
2008-10-24 18:46:09 +0200

    Redirecting all expired sessions and linked urls to frontpage

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,9 @@
 class PhotosController < ApplicationController
-  before_filter :lookup_photo, :only => [:show, :edit, :update, :destroy]
+  before_filter :lookup_photo, :only => [:show, :edit, :update]
 
   # GET /photos/1
   # GET /photos/1.xml
   def show
-
     respond_to do |format|
       format.html { @photo.generate unless @photo.exists? }
       format.jpg  { render_preview(@photo) }
@@ -50,6 +49,7 @@ class PhotosController < ApplicationController
   # PUT /photos/1
   # PUT /photos/1.xml
   def update
+		
     respond_to do |format|
       if @photo.update_attributes(params[:photo])
         # crop *and replace* existing image
@@ -62,24 +62,13 @@ class PhotosController < ApplicationController
       end
     end
   end
-
-  # DELETE /photos/1
-  # DELETE /photos/1.xml
-  def destroy
-    @photo.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(photos_url) }
-      format.xml  { head :ok }
-    end
-  end
   
   def download
     photo = Photo.find(params[:id])
-    
     send_file photo.full_filename(:final), :type => 'image/jpg', :disposition => 'attachment'
   end
 
+	# helps fixing upload issues for Safari browser
   def closekeepalive
     response.headers['Connection'] = 'Close'
     render :text => ''
@@ -87,12 +76,16 @@ class PhotosController < ApplicationController
 
   
   private
-  
   def render_preview(photo)
     send_file photo.full_filename(:preview), :type => 'image/jpg', :disposition => 'inline'
   end
   
   def lookup_photo
+		if session[:photo_id].blank?
+			flash[:notice] = 'Dit BilledId blev ikke fundet. Systemet sletter automatisk gamle BilledID. Du kan lave et nyt BilledId med det samme.'
+			redirect_to root_url
+			return
+		end
     @photo = Photo.find(session[:photo_id])
   end
   
