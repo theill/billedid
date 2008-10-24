058429df19ae89677e872b4816b1955b9a4eaf37
Peter Theill <peter@theill.com>
2008-10-24 13:28:21 +0200

    adding about controller with views for terms, privacy and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 ActionController::Routing::Routes.draw do |map|
+  
+  def map.controller_actions(controller, aktions)
+    aktions.each do |action|
+      self.send("#{controller}_#{action}", "#{action}", :controller => controller, :action => action)
+    end
+  end
+
   map.resources :photos, :member => {:download => :get}
   
   map.namespace :admin do |admin|
     admin.resources :photos
   end
-
+  
+  map.controller_actions 'about', %w[terms privacy about]
+  
   map.root :controller => 'photos', :action => 'new'
 end
