b54356ec87eba7fbd15ef96424b579fc27b1fc60
Morgan Roderick <morgan@MorgBook.local>
2008-10-24 19:08:35 +0200

    Cleanup of javascript and image tags

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,3 +7,35 @@ if ( /AppleWebKit|MSIE/.test( navigator.userAgent ) ) {
 	  }
 	});
 }
+
+Event.onReady( function(){
+	var pageTracker = _gat._getTracker("UA-869742-15");
+	pageTracker._trackPageview();
+});
+
+Event.onReady( function() {
+	if ($('photo-image')){
+		new Cropper.ImgWithPreview(
+			'photo-image', {
+				previewWrap: 'photo-preview',
+				minWidth: 70,
+				minHeight: 90,
+				ratioDim: {
+					x: 70, 
+					y: 90
+				},
+				displayOnInit: true,
+				onEndCrop: onEndCrop
+			}
+		);	
+	}
+});
+	
+function onEndCrop(coords, dimensions) {
+	$('x1').value = coords.x1;
+	$('y1').value = coords.y1;
+	$('x2').value = coords.x2;
+	$('y2').value = coords.y2;
+	$('width').value = dimensions.width;
+	$('height').value = dimensions.height;
+}
