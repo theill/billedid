a947ec4a99398a747e79a78117e65aa030a90f9b
Peter Theill <peter@theill.com>
2010-01-20 14:23:28 +0100

    upgraded to new hoptoad notifier version

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   config.gem "aws-s3", :lib => "aws/s3", :version => '0.6.2'
+  config.gem "hoptoad_notifier", :version => '2.1.0'
   # config.gem "rmagick", :version => '>=1.15.17'
   # config.gem 'mime-types', :lib => 'mime/types'
 
