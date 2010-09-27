94a334c7e88df671652277744dfa98181c4eadf3
Peter Theill <peter@theill.com>
2010-09-27 09:44:25 +0200

    handle method not allowed errors by returning a 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::RoutingError, :with => :route_not_found
+  rescue_from ActionController::MethodNotAllowed, :with => :invalid_method
   helper :all
 
   protect_from_forgery
+  
+  private
+  
+  def route_not_found
+    render :file => "#{Rails.root}/public/404.html", :status => "404"
+  end
+  
+  def invalid_method
+    render :file => "#{Rails.root}/public/404.html", :status => "404"
+  end  
   
 end
